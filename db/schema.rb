6d30f1bc8728049389ff5e910710d2c3489635b8
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-31 11:45:52 +0900

    adminモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_022935) do
+ActiveRecord::Schema.define(version: 2020_08_31_024514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
