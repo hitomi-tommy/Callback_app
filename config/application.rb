f8f3ee2a1fa11a05160ae6f454ed097a8176cb95
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-31 11:23:47 +0900

    git init

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module CallbackApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    config.generators do |g|
+          g.assets     false
+          g.helper     false
+        end
+  end
+end
